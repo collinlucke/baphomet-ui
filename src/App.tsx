3aa778e01a3deb9eaaa3c400b2a901f0f3e9235f
Collin Lucke <collin.lucke@gmail.com>
2024-07-20 13:41:10 -0600

    Got rid of tailwind because https://rb.gy/beb4ko

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header.tsx';
 import './App.css';
@@ -6,7 +5,7 @@ import './App.css';
 // TODO: Make error handling and loading states better
 function App() {
   return (
-    <div className="w-full bg-blue-400">
+    <div>
       <Header />
       <Outlet />
     </div>
