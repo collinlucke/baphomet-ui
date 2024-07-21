3f580854b2e1da99f99bd02e99f3cb62f1c7510c
Collin Lucke <collin.lucke@gmail.com>
2024-07-21 17:04:46 -0600

    St-st-st-st-st- Stylex!!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header.tsx';
+import './styles.css';
 import './App.css';
-// import '../../phantomartist/dist/style.css';
+
 // TODO: Make error handling and loading states better
 function App() {
   return (
