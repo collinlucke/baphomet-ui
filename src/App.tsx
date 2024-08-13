eb7af8963b2a01c071ccc766dc8a5a126258e149
Collin Lucke <collin.lucke@gmail.com>
2024-08-13 17:32:51 -0600

    The kinks persist...

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,6 @@ import { Heading } from './components/Heading.tsx';
 import { Main } from '@crazy-overlord/phantomartist';
 import './styling/styleX.css';
 
-const base_url =
-  process.env.NODE_ENV === 'development'
-    ? process.env.LOCAL_BASE_URL
-    : process.env.SERVER_BASE_UR;
-
 function App() {
   return (
     <div>
