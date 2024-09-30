e420de38bf3ce4ef4f8c45357a3f76a3bb7abf4d
Collin Lucke <collinlucke@gmail.com>
2024-09-30 12:11:53 -0600

    Updates from prod

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import routes from './routes';
 
 const baseURL =
-  import.meta.env.VITE_NODE_ENV === 'development'
-    ? import.meta.env.VITE_LOCAL_BASE_URL
-    : import.meta.env.VITE_SERVER_BASE_URL;
+  import.meta.env.NODE_ENV === 'development'
+    ? import.meta.env.VITE_DEV_URI
+    : import.meta.env.VITE_PROD_URI;
 
-const router = createBrowserRouter(routes);
+const router = createHashRouter(routes);
 
 const client = new ApolloClient({
   uri: `${baseURL}/graphql/`,
   cache: new InMemoryCache()
 });
-
-// TODO: This is prolly gonna get moved to it's own file
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
