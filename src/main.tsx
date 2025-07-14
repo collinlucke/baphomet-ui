66903736876a63a375c993125de6d812c17ba206
collinlucke <collin.lucke@gmail.com>
2025-07-14 07:50:35 -0600

    Try to make that .htaccess a thing

--- src/main.tsx
+++ src/main.tsx
@@ -50,3 +50,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </ApolloProvider>
   </React.StrictMode>
 );
+
+// Handle redirect from 404.html
+const redirectPath = sessionStorage.getItem('redirectPath');
+if (redirectPath) {
+  sessionStorage.removeItem('redirectPath');
+  window.history.replaceState(null, '', redirectPath);
+}
