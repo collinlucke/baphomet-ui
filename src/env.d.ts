1573c8b61e10d70dfb2522cab60cc458bf87a24a
collinlucke <collin.lucke@gmail.com>
2025-07-15 16:32:56 -0600

    Update configs

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,10 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly VITE_GRAPHQL_BAPHOMET_SERVER_RENDER_URL: string;
   readonly GRAPHQL_BAPHOMET_SERVER_RENDER_URL: string;
+  readonly GIT_REGISTRY_TOKEN: string;
+  readonly MODE: string;
   // more env variables...
 }
 
