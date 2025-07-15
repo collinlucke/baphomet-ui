28360970a696dbf9cf2f1c078397baf2b1f2d760
collinlucke <collin.lucke@gmail.com>
2025-07-15 15:05:04 -0600

    Configure app for path-based deployment at /baphomet instead of subdomain

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(async ({ mode }) => {
   return {
+    base: '/baphomet/',
     plugins: [
       react({
         jsxImportSource: '@emotion/react',
